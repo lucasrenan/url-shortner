3f0c870c52fd3ab03ec512a0f1445b8625f7d7b1
lucas renan <contato@lucasrenan.com>
2018-02-11 14:49:15 +0100

    Name the shortened route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   resources :urls, only: [:create, :destroy]
 
-  get '/:token', to: 'urls#show', constraints: { token: /[0-9a-fA-F]{7,8}/ }
+  get '/:token', to: 'urls#show',
+                 constraints: { token: /[0-9a-fA-F]{7,8}/ },
+                 as: :shortened
 end
